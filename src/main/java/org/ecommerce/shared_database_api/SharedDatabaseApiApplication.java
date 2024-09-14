94df9d7e0d9a0c5dc89d8ec9460c31eaff1a4a11
anil-chauhan <anil.rajput1984@gmail.com>
2024-09-14 17:49:15 +0530

    first commit

---
+++ src/main/java/org/ecommerce/shared_database_api/SharedDatabaseApiApplication.java
@@ -0,0 +1,13 @@
+package org.ecommerce.shared_database_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedDatabaseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharedDatabaseApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ecommerce/shared_database_api/SharedDatabaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecommerce.shared_database_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedDatabaseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
