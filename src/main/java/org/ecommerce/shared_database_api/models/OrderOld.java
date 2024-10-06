96af9f7da0d3a2aac3d67365c2fbaf6fc3c1301d
anil-chauhan <anil.rajput1984@gmail.com>
2024-10-06 17:54:54 +0530

    save purchase order and customer, shipping, billing ,orders item details in to a database

---
+++ src/main/java/org/ecommerce/shared_database_api/models/OrderOld.java
@@ -0,0 +1,58 @@
+/*
+package org.ecommerce.shared_database_api.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "orders")
+@Data
+public class OrderOld {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id", nullable = false)
+    private Integer orderId;
+
+    @Column(name = "order_number", nullable = false, length = 50)
+    private String orderNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.SET_NULL)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "total_amount", nullable = false, precision = 10, scale = 2)
+    private BigDecimal totalAmount;
+
+    @Column(name = "discount_amount", nullable = false, precision = 10, scale = 2)
+    private BigDecimal discountAmount;
+
+    @Column(name = "gross_amount", nullable = false, precision = 10, scale = 2)
+    private BigDecimal grossAmount;
+
+    @Column(name = "shipping_amount", nullable = false, precision = 10, scale = 2)
+    private BigDecimal shippingAmount;
+
+    @Column(name = "net_amount", nullable = false, precision = 10, scale = 2)
+    private BigDecimal netAmount;
+
+    @Column(name = "status", nullable = false, length = 20)
+    private String status;
+
+    @Column(name = "payment_status", nullable = false, length = 20)
+    private String paymentStatus;
+
+    @Column(name = "payment_type", nullable = false, length = 20)
+    private String paymentType;
+
+    @Column(name = "payment_transaction_id", length = 100)
+    private String paymentTransactionId;
+
+
+
+}
+*/
